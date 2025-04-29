75cb7cfdf33eb7f840582c9ea88fbada6970d806
muddassirali8089 <114619571+muddassirali8089@users.noreply.github.com>
2025-04-29 23:45:14 +0500

    picture ADDED etc

--- MuddassirPortfolio/src/Components/LeftSideBar.jsx
+++ MuddassirPortfolio/src/Components/LeftSideBar.jsx
@@ -7,32 +7,33 @@ const LeftSideBar = () => {
       <img  style ={{width:"130px"  , height:"70px"}}src="../../public/assets/images/MuddassirLogo1.png" alt="Logo" />
       <span className="designation">MERN STACK DEVELOPER</span>
     </div>
-    <img className="me" src="assets/images/me.jpg" alt="Me" />
-    <h2 className="email">hello@drake.design</h2>
-    <h2 className="address">Base in Los Angeles, CA</h2>
-    <p className="copyright">© 2022 Drake. All Rights Reserved</p>
+    <img  style={{height:"160px"}} className="me" src="assets/images/muddassir.jpg" alt="Me" />
+    <h2 style={{fontSize:"20px"}} className="email">muddassirali8089@gmail.com</h2>
+    <h2 className="address">Islamabad , Pakistan</h2>
+    <p className="copyright">© {new Date().getFullYear()} Muddassir. All Rights Reserved</p>
     <ul className="social-profile d-flex align-items-center flex-wrap justify-content-center">
-      <li>
-        <a href="#">
-          <i className="lab la-twitter" />
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <i className="lab la-dribbble" />
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <i className="lab la-instagram" />
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <i className="lab la-github" />
-        </a>
-      </li>
-    </ul>
+  <li>
+    <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
+      <i className="lab la-twitter" />
+    </a>
+  </li>
+  <li>
+    <a href="https://www.linkedin.com/in/your-username/" target="_blank" rel="noopener noreferrer">
+      <i className="lab la-linkedin" />
+    </a>
+  </li>
+  <li>
+    <a href="https://instagram.com/yourusername" target="_blank" rel="noopener noreferrer">
+      <i className="lab la-instagram" />
+    </a>
+  </li>
+  <li>
+    <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+      <i className="lab la-github" />
+    </a>
+  </li>
+</ul>
+
     <a href="#" className="theme-btn">
       <i className="las la-envelope" /> Hire Me!
     </a>
